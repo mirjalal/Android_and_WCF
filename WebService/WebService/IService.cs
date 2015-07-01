54c51602c3920de0b70d4035645704b5815127ab
protectedd <talish403@outlook.com>
2015-07-01 18:12:48 +0500

    upppss
    some bugs fixed. "web service changed for getting images"

--- WebService/WebService/IService.cs
+++ WebService/WebService/IService.cs
@@ -3,13 +3,14 @@ using System.ServiceModel.Web;
 
 namespace WebService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
     [ServiceContract]
-    [XmlSerializerFormat]
     public interface IService
     {
         [OperationContract]
-        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, UriTemplate = "login?istiad={istiad}", BodyStyle = WebMessageBodyStyle.Bare)]
-        string Login(string istiad/*, string sifre*/);
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "login?istiad={istiad}&sifre={sifre}")]
+        string Login(string istiad, string sifre);
+
+        [WebGet(UriTemplate = "GetImage", RequestFormat = WebMessageFormat.Xml, ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare)]
+        System.IO.Stream GetImage ();
     }
 }
--- WebService/WebService/Service.svc.cs
+++ WebService/WebService/Service.svc.cs
@@ -1,16 +1,20 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Data;
+using System.Drawing;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Web;
 
 namespace WebService
 {
     public class Service : IService
     {
-        public string Login(string istiad/*, string sifre*/)
+        public string Login(string istiad, string sifre)
         {
             DataAccess dataAccess = new DataAccess();
 
-            return dataAccess.checkLogin(istiad/*, sifre*/);
+            return dataAccess.checkLogin(istiad, sifre);
         }
 
 
@@ -20,11 +24,11 @@ namespace WebService
 
             public DataAccess()
             {
-                connection = new MySqlConnection("server=localhost;uid=root;password=connection;database=erp");
+                connection = new MySqlConnection("server=45.35.4.29;uid=root;password=connection;database=erp");
             }
 
 
-            public string checkLogin(string istiad/*, string sifre*/)
+            public string checkLogin(string istiad, string sifre)
             {
                 if (connection.State == ConnectionState.Closed)
                     connection.Open();
@@ -37,6 +41,14 @@ namespace WebService
                 else
                     return "0";
             }
+        }
+
+
+        public Stream GetImage ()
+        {
+            FileStream fs = File.OpenRead(@"D:\a.jpg");
+            WebOperationContext.Current.OutgoingResponse.ContentType = "image/jpeg";
+            return fs;
         }
     }
 }
